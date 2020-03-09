21e115331a17580f6afdd962d4924dcb1760b19c
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-03-09 08:34:10 -0400

    ajout zip

---
+++ create-zip.bat
@@ -0,0 +1,14 @@
+echo off
+set zipName=filter_recitactivity
+set pluginName=filter_recitactivity
+
+rem remove the current 
+del %zipName%
+
+rem zip the folder
+"c:\Program Files\7-Zip\7z.exe" a -mx "%zipName%.zip" "src\*"
+
+rem set the plugin name
+"c:\Program Files\7-Zip\7z.exe" rn "%zipName%.zip" "src\" "%pluginName%\"
+
+pause
