c9b96aa0c058fb959bb8b89351b6469636bf989e
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-04-02 13:49:04 -0400

    r6

--- src/version.php
+++ src/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020022900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020040100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'filter_recitactivity'; // Full name of the plugin (used for diagnostics)
-$plugin->release = 'R5-2020022900';
+$plugin->release = 'R6-2020040100';
