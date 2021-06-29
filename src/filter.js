1c5c00280564df59f3e67b721bd63cff81aabce1
Émile Jacques <emilevirus@gmail.com>
2021-06-29 09:24:18 -0400

    Fix js errors for other themes

--- src/filter.js
+++ src/filter.js
@@ -75,7 +75,8 @@ recit.filter.autolink.popupIframe = function(url){
             content.contentWindow.document.querySelector('nav').style.display = 'none'; //remove navbar
             content.contentWindow.document.querySelector('header').style.display = 'none'; //remove course header
             if (content.contentWindow.document.querySelector('#sidepreopen-control')) content.contentWindow.document.querySelector('#sidepreopen-control').style.display = 'none'; //remove sidebar icon drawer
-            content.contentWindow.document.querySelector('#top-footer1').style.display = 'none'; //remove footer
+            if (content.contentWindow.document.querySelector('#nav-drawer')) content.contentWindow.document.querySelector('#nav-drawer').style.display = 'none'; //remove sidebar icon drawer
+            if (content.contentWindow.document.querySelector('footer')) content.contentWindow.document.querySelector('footer').style.display = 'none'; //remove footer
         }
         //content.style.height = content.contentWindow.document.documentElement.scrollHeight + 'px'; //adjust iframe to page height
         popup.update();
