33307abb6ab2e703f41313f2ef04f500e95682fa
Gustavo Bazzo <gcbazzo@gmail.com>
2021-07-12 13:39:56 -0400

    Finish to change to Moodle DB functions

--- src/classes/dao.php
+++ src/classes/dao.php
@@ -1,34 +1,19 @@
 <?php
 
-interface i_filter_recitautolink_dao{
-	public function load_course_teachers($courseid);
-    public function load_cm_completions($courseId);
-}
-
-abstract class dao_filter_recitautolink implements i_filter_recitautolink_dao{
-    protected $dbconn = null;
-
-    protected function setDbConn($propName){
-        global $DB;
-
-		$moodleDB = $DB;
-		$refMoodleDB = new ReflectionObject($moodleDB);
-		$refProp1 = $refMoodleDB->getProperty($propName);
-		$refProp1->setAccessible(TRUE);
-		$this->dbconn = $refProp1->getValue($moodleDB);
-    }
-
+class dao_filter_recitautolink{
      /**
      * This function gets all teachers for a course.
      *
      * @param int $courseid
      */
     public function load_course_teachers($courseid, $group = false){
-        global $CFG, $USER, $DB;
+        global $USER, $DB;
 
         $where = "where t3.courseid = :courseid";
         
-        $query = "select t1.id as id, t1.firstname, t1.lastname, t1.email, t5.shortname as role, concat(t1.firstname, ' ', t1.lastname) as imagealt,
+        $DB->execute("set @uniqueId = 0");
+
+        $query = "select  @uniqueId := @uniqueId + 1 as uniqueId, t1.id as id, t1.firstname, t1.lastname, t1.email, t5.shortname as role, concat(t1.firstname, ' ', t1.lastname) as imagealt,
         t1.picture, t1.firstnamephonetic, t1.lastnamephonetic, t1.middlename, t1.alternatename   
         from {user} as t1  
         inner join {user_enrolments} as t2 on t1.id = t2.userid
@@ -47,28 +32,7 @@ abstract class dao_filter_recitautolink implements i_filter_recitautolink_dao{
         $result = array();
 		foreach($rst as $obj){
             $result[] = $obj;
-        }
-
-        /*$result = array();
-        foreach(array('teacher', 'editingteacher', 'noneditingteacher') as $r){
-            $role = $DB->get_record('role', array('shortname' => $r));
-            if ($role){
-                $context = get_context_instance(CONTEXT_COURSE, $courseid);
-                $teachers = get_role_users($role->id, $context);
-                foreach($teachers as $t){
-                    if (!$group){
-                        $result[] = $t;
-                    }else{
-                        $groupd = $DB->get_record('groups_members', array('userid' => $t->id));
-                        $user_group = $DB->get_record('groups_members', array('userid' => $USER->id));
-                        if ($groupd && $user_group && $groupd->groupid == $user_group->groupid){
-                            $result[] = $t;
-                        }
-                    }
-                }
-            }
-        }*/
-        
+        }    
 
         return $result;
     }
@@ -89,37 +53,6 @@ abstract class dao_filter_recitautolink implements i_filter_recitautolink_dao{
 
         return $result;
     }
-
-    abstract protected function exec_query($query);
-    abstract protected function fetch_object($rst);
-}
-
-class mysql_filter_recitautolink extends dao_filter_recitautolink{
-    public function __construct(){
-        $this->setDbConn('mysqli');
-    }
-
-    protected function exec_query($query){
-        return $this->dbconn->query($query);
-    }
-
-    protected function fetch_object($rst){
-        return $rst->fetch_object();
-    }
-}
-
-class postgresql_filter_recitautolink extends dao_filter_recitautolink{
-    public function __construct(){
-        $this->setDbConn('pgsql');
-    }
-
-    protected function exec_query($query){
-        return pg_query($this->dbconn, $query);
-    }
-
-    protected function fetch_object($rst){
-        return pg_fetch_object($rst);
-    }
 }
 
 class dao_filter_recitautolink_factory{
@@ -134,14 +67,6 @@ class dao_filter_recitautolink_factory{
 	}
  
 	public function getDAO(){
-        global $CFG;
-
-        switch($CFG->dbtype){
-            case "pgsql":
-                return new postgresql_filter_recitautolink();
-            case "mysqli":
-            default:
-                return new mysql_filter_recitautolink();
-	}
+        return new dao_filter_recitautolink();
 	}
 }
--- src/version.php
+++ src/version.php
@@ -30,5 +30,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021050400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800;        // Requires this Moodle version.
 $plugin->component = 'filter_recitactivity'; // Full name of the plugin (used for diagnostics).
-$plugin->release = 'R13-V1.13.1';
+$plugin->release = 'R13-V1.13.2';
 $plugin->maturity = MATURITY_BETA;
