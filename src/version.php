2cdf96eb695b156f4fccf992f0d1f4e6e49e1c60
Gustavo Bazzo <gcbazzo@gmail.com>
2021-09-24 11:53:05 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021071200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021092400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800;        // Requires this Moodle version.
 $plugin->component = 'filter_recitactivity'; // Full name of the plugin (used for diagnostics).
-$plugin->release = 'R13-V1.13.2';
+$plugin->release = 'R14-V1.0';
 $plugin->maturity = MATURITY_BETA;
