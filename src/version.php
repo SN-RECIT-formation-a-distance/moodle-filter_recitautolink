fed7b4df2b231a5e356f9c598778dea6f0561d70
Émile Jacques <emilevirus@gmail.com>
2022-01-10 11:35:38 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021111500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018050800;        // Requires this Moodle version.
+$plugin->requires  = 2020060700;        // Requires this Moodle version.
 $plugin->component = 'filter_recitactivity'; // Full name of the plugin (used for diagnostics).
 $plugin->release = 'R14-V1.5';
 $plugin->maturity = MATURITY_BETA;
