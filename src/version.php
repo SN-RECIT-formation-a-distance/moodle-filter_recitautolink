8a1aa27347bb9df703689de3dc421ae4341d2e41
Gustavo Bazzo <gcbazzo@gmail.com>
2025-02-17 11:57:16 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -29,6 +29,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2025013000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024071200.00; // Moodle 4.5.0
 $plugin->component = 'filter_recitactivity'; // Full name of the plugin (used for diagnostics)
-$plugin->release = 'v2.0.1-stable';
+$plugin->release = 'v2.0.2-stable';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];      //  Moodle 3.9.x, 3.10.x, 3.11.x and 4.0.x are supported.
